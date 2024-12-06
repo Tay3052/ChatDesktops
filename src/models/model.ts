6c202c154b632d2a2c9d1f69e24cc422c954a241
Takumi <fd3s.bcnr33@gmail.com>
2024-12-06 10:27:18 +0900

    creating SQL

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,10 +1,12 @@
 import { pgTable, serial, text, uuid, unique, date } from "drizzle-orm/pg-core";
+import { type User } from "./interfaces/modelInterfaces";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey(),
-  name: text("name").unique().notNull(),
-  email: text("email").unique(),
+  username: text("username").unique().notNull(),
+  email: text("email").unique().notNull(),
   password: text("password").notNull(),
+  pgpkey: text("pgpkey").notNull(),
   createdAt: date("created_at").notNull(),
   updatedAt: date("updated_at").notNull(),
 });
